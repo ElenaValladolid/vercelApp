9e14c3165d48a860d56d350fe43fc64b5274ff36
ElenaValladolid <ehmenoyo@gmail.com>
2024-07-04 16:40:53 +0200

    First commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api', (req, res)=>{
+    res.send("Hola caracola");
+});
+
+app.listen(port, ()=>{
+    console.log(`Server ejecutando el el puerto ${port}`);
+});
+
+
